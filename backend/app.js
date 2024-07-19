d36ecf6f12f4abf933a37de83caf8467a84f48f0
Ali Hassan <145183956+Alihassan699@users.noreply.github.com>
2024-07-19 18:20:58 +0500

    version10

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,24 @@
-import express from 'express';
+// app.js
 import dotenv from 'dotenv';
-import cors from 'cors';
-import pg from 'pg';
-
 dotenv.config();
-
-const db = new pg.Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'quiz_Application',
-    password: 'Al!h@ss@n',
-    port: 5432
-});
-
-db.connect();
+import bodyParser from 'body-parser';
+import express from 'express';
+import { connectdb } from './db/connect.js';
 
 const app = express();
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+connectdb();
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
